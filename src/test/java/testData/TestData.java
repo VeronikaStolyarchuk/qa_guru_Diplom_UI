15d93a42e623ca502ef1523b2e5402275b133909
VeronikaStolyarchuk <veronika.stolyarchuk@yandex.ru>
2026-09-23 09:14:13 +0300

    Update TestData.java

--- src/test/java/testData/TestData.java
+++ src/test/java/testData/TestData.java
@@ -11,7 +11,7 @@ public class TestData {
     public String USER_INVALID_PHONE = "12345";
     public String SUCCESS_TEXT_OF_ORDER = "Спасибо, Ваша заявка принята: ";
     public String ERROR_TEXT_OF_PHONE_NUMBER = "Пожалуйста, Введите ваш мобильный телефон";
-    public String ERROR_TEXT_OF_EMPTY_PASSWORD = "Заполните это поле.";
+    public String ERROR_TEXT_OF_EMPTY_PASSWORD = "Please fill out this field.";
     public String REMOVE_CONFIRMATION_MESSAGE  = "Вы уверены, что хотите удалить этот товар из корзины?";
     public String EMPTY_CART_MESSAGE  = "Чтобы продолжить оформление заказа пожалуйста добавьте в заказ букет или цветы из";
 }
